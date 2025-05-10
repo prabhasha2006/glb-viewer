6f03e41d7e32b94b5341cd76ccfa9f6f0a56ee3d
Evelocore <kumuthuprabhasha@gmail.com>
2025-05-10 14:14:12 +0530

    vite + vanila

--- viewer.js
+++ viewer.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 // Get canvas container
 const canvasContainer = document.getElementById('canvas-root');
